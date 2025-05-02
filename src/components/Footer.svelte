2023fb5ad083028aab1c55a4ffac65b23679aee7
Pamodi1022 <pamodipansiluni1022@gmail.com>
2025-05-02 15:44:49 +0530

    Add Footer component to layout for copyright information

---
+++ src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<script>
+    const currentYear = new Date().getFullYear();
+</script>
+
+<footer class="bg-gray-100 dark:bg-gray-800 py-4 mt-8">
+    <div class="container mx-auto px-4 text-center text-gray-600 dark:text-gray-300">
+        <p>&copy; {currentYear} Budget Buddy. All rights reserved.</p>
+    </div>
+</footer> 
